4d9d4d808d1c963ae8b459995c95053dc483dae0
Torsten Bergmann <astares@gmx.de>
2018-08-18 01:56:25 +0200

    add struct for VM

--- src/rr.c
+++ src/rr.c
@@ -5,6 +5,10 @@
 
 int main(int argc, char **argv)
 {
-	printf("Roadrunner\n");
+	VirtualMachine* vm;
+	vm = (VirtualMachine*) calloc(1, sizeof(VirtualMachine));
+	
+	vm->flags = 18;
+	printf("Roadrunner: flags %i\n", vm->flags);
 	return 0;
 }
